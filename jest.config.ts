3e97bc7a5908537931cf30a10ab6e46b573c12a2
dogyeong <shroad1802@gmail.com>
2023-10-02 19:46:16 +0900

    jest 설정

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,13 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  rootDir: __dirname,
   verbose: true,
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleDirectories: ['node_modules', 'src'],
 };
 
 export default config;
