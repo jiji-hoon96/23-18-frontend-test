114a6701755b6a0cf4e6b630a081a0282cedec99
dogyeong <shroad1802@gmail.com>
2023-10-02 19:26:45 +0900

    prettier, eslint 설정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { App } from "./App";
+import { App } from './App';
 
 describe('App', () => {
   it('should render without crashing', () => {
     render(<App />);
 
     expect(screen.getByText('Hello, World!')).toBeInTheDocument();
-  })
-})
+  });
+});
