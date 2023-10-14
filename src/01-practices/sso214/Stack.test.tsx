d5d9b68a2b2ba8f39947ee2550a2f01e376d1af3
sso214 <sojam214@gmail.com>
2023-10-15 00:02:19 +0900

    fix : ESLint 관련 에러 수정

--- src/01-practices/sso214/Stack.test.tsx
+++ src/01-practices/sso214/Stack.test.tsx
@@ -5,7 +5,7 @@ import Stack from './Stack';
 function renderStack() {
   const DATA = ['첫번째 손님', '두번째 손님', '세번째 손님'];
 
-  const stack = render(<Stack elements={DATA} />)
+  const stack = render(<Stack elements={DATA} />);
 
   const PopButton = () => stack.getByTestId('button');
   const StackList = () => stack.getByTestId('list');
@@ -26,11 +26,11 @@ function renderStack() {
     StackItems,
 
     clickPopButton,
-  }
+  };
 }
 
-describe('pop 기능이 있는 Stack 컴포넌트 구현',() => {
-  it('Stack을 처음 렌더링했을 때 화면이 올바르게 노출된다.', () => {
+describe('pop 기능이 있는 Stack 컴포넌트 구현', () => {
+  it('<Stack />을 처음 렌더링했을 때 화면이 올바르게 노출된다.', () => {
     const { DATA, PopButton, StackList, StackItems } = renderStack();
 
     expect(PopButton()).toBeInTheDocument();
@@ -39,9 +39,9 @@ describe('pop 기능이 있는 Stack 컴포넌트 구현',() => {
     expect(StackList()).toHaveTextContent(DATA[1]);
     expect(StackList()).toHaveTextContent(DATA[2]);
     expect(StackItems().length).toBe(3);
-  })
+  });
 
-  it('pop 버튼 클릭 시 리스트 마지막 순서의 item이 제거된다. ', async () => {
+  it('pop 버튼 클릭 시 리스트 마지막 순서의 item 이 제거된다.', async () => {
     const { clickPopButton, StackItems, StackList, DATA } = renderStack();
 
     await clickPopButton();
@@ -50,5 +50,5 @@ describe('pop 기능이 있는 Stack 컴포넌트 구현',() => {
     expect(StackList()).toHaveTextContent(DATA[0]);
     expect(StackList()).toHaveTextContent(DATA[1]);
     expect(StackList()).not.toHaveTextContent(DATA[2]);
-  })
-})
+  });
+});
--- src/01-practices/sso214/Stack.tsx
+++ src/01-practices/sso214/Stack.tsx
@@ -4,25 +4,29 @@ interface Props {
   elements: string[];
 }
 
-function Stack({elements}: Props) {
+function Stack({ elements }: Props) {
   const [state, setState] = useState(elements);
 
   const handlePop = () => {
     const list = [...state];
     list.pop();
     setState(list);
-  }
+  };
 
   return (
     <>
-      <button data-testid='button' onClick={handlePop}>pop</button>
-      <ul data-testid='list'>
+      <button data-testid="button" onClick={handlePop}>
+        pop
+      </button>
+      <ul data-testid="list">
         {state.map((item) => (
-          <li key={item} data-testid='item'>{item}</li>
+          <li key={item} data-testid="item">
+            {item}
+          </li>
         ))}
       </ul>
     </>
-  )
+  );
 }
 
 export default Stack;
