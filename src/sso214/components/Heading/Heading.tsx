de72b7ea7e0e30b25685002424979e43cc4e9cd4
sso214 <sojam214@gmail.com>
2023-10-16 21:49:33 +0900

    feat : Heading 컴포넌트 생성

---
+++ src/sso214/components/Heading/Heading.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import S from './style.module.css';
+
+
+type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+interface Props extends React.HTMLAttributes<HTMLHeadingElement>{
+  headingLevel?: HeadingLevel;
+}
+
+const Heading = ({headingLevel = 'p', children, className = '', ...res}: Props) => {
+  const Heading = ({...props}: React.HTMLAttributes<HTMLHeadingElement>) => React.createElement(`h${headingLevel}`, props, children);
+  const headingStyle: Record<HeadingLevel, string> = {
+    'h1': S.h1,
+    'h2': S.h2,
+    'h3': S.h3,
+    'h4': S.h4,
+    'h5': S.h5,
+    'h6': S.h6,
+    'p': S.p,
+  };
+
+  return <Heading className={[headingStyle['h1'], className].join(' ')} {...res}>{children}</Heading>
+}
+
+
+export default Heading;
