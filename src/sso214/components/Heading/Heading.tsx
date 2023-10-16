dd6f085bafec6a4463654251b151c85fd77eac86
sso214 <sojam214@gmail.com>
2023-10-16 22:28:35 +0900

    feat : Heading 컴포넌트 headingLevel props 필수로 수정

--- src/sso214/components/Heading/Heading.tsx
+++ src/sso214/components/Heading/Heading.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import S from './style.module.css';
 
-
 type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
-interface Props extends React.HTMLAttributes<HTMLHeadingElement>{
-  headingLevel?: HeadingLevel;
+interface Props extends React.HTMLAttributes<HTMLHeadingElement> {
+  headingLevel: HeadingLevel;
 }
 
-const Heading = ({headingLevel = 'p', children, className = '', ...res}: Props) => {
-  const Heading = ({...props}: React.HTMLAttributes<HTMLHeadingElement>) => React.createElement(`h${headingLevel}`, props, children);
+const Heading = ({ headingLevel, children, className = '', ...res }: Props) => {
+  const Heading = ({ ...props }: React.HTMLAttributes<HTMLHeadingElement>) =>
+    React.createElement(headingLevel, props, children);
   const headingStyle: Record<HeadingLevel, string> = {
-    'h1': S.h1,
-    'h2': S.h2,
-    'h3': S.h3,
-    'h4': S.h4,
-    'h5': S.h5,
-    'h6': S.h6,
-    'p': S.p,
+    h1: S.h1,
+    h2: S.h2,
+    h3: S.h3,
+    h4: S.h4,
+    h5: S.h5,
+    h6: S.h6,
+    p: S.p,
   };
 
-  return <Heading className={[headingStyle['h1'], className].join(' ')} {...res}>{children}</Heading>
-}
-
+  return (
+    <Heading className={`${headingStyle[headingLevel]} ${className}`} {...res}>
+      {children}
+    </Heading>
+  );
+};
 
 export default Heading;
